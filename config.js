3efcf09b153dd3a750c0ffc186232cf82d80bd75
jonnysoco <jonnysoco@gmail.com>
2020-09-30 15:09:31 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const Twit = require('twit');
+const TH = new Twit({
+    consumer_key:         'hKaqMBYQAOMpKlXgzhP7AWywF',
+  consumer_secret:      'PHxG8Uws3TZjEDbdOpg1S4ozWjkwf7CWWF8YrUU8BzQGv4gehW',
+  access_token:         '424248995-CWtFOXlRfmDzwcfEivfokHzfpX8JKllCCp2iqXkF',
+  access_token_secret:  'v09ZxFKMICjHXy8qUH0xn9dUdC2AB4cOlB1sPvx0bvXgc'
+});
+
+
